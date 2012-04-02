fd40af1a24fe90d7c31d40876071327d16d839b6
Secure User <alter@anote.localdomain>
2012-04-03 01:17:51 +0400

    ready script for earning money in minitroopers.com

---
+++ troopers.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function check {
+    message=`egrep "(Another|Shortage)" index`
+    if [ "$message" == "Shortage" ]
+    then
+        return 0
+    fi
+}
+
+prefix="/home/alter/troopers/"
+login=$1
+password=$2
+curl -c ${prefix}cookie.$login -d "login=$login&pass=$password" http://$login.minitroopers.com/login
+curl -b ${prefix}cookie.$login http://$login.minitroopers.com/hq > index
+check
+
+while [ $message == "Another" ]
+do
+    key=`egrep -o -e "chk=[A-Za-z0-9]{6}" index |tail -n1`
+    curl -b ${prefix}cookie.$login http://$login.minitroopers.com/b/raid?$key
+    curl -b ${prefix}cookie.$login http://$login.minitroopers.com/hq > index
+#    money=`grep money index -A1|tail -n1`
+#    echo "$login earn $money"
+    check
+done
+
+rm -f index
