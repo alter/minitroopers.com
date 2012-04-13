e9a30d5fa5f2290e21b066141c24f0790b786fa5
alter <alter.pub@gmail.com>
2012-04-14 02:26:29 +0400

    added possibility to play with users without passwords, also a little bit refactored code

--- troopers.sh
+++ troopers.sh
@@ -3,12 +3,14 @@
 prefix="/home/alter/troopers/" # prefix should be with "/" in the end
 login=$1                       # 1st argument of cli
 password=$2                    # 2nd argument of cli
+exit_cycle=0
 
 # Check for "raids"
 function check {
     message=`egrep "(Another|Shortage)" index`
-    if [ "$message" == "Shortage" ]
+    if [ "$message" == "Shortage" ] || [ -z "$message" ]
     then
+        exit_cycle=1
         mission
     fi
 }
@@ -33,20 +35,25 @@ function mission {
 function fight {
     for i in {1..3}
     do
-        curl -b ${prefix}cookie.$login http://$login.minitroopers.com/b/opp > opp$i
-        fight_key=`egrep -o -e "opp=[0-9]{5,7};chk=[a-zA-Z0-9]{6}" opp$i|head -n1`
+        curl -b ${prefix}cookie.$login http://$login.minitroopers.com/b/opp > opp
+        fight_key=`egrep -o -e "opp=[0-9]{5,7};chk=[a-zA-Z0-9]{6}" opp|head -n1`
         echo $fight_key
         curl -b ${prefix}cookie.$login http://$login.minitroopers.com/b/battle?$fight_key
     done
 }
 
 # Login
-curl -c ${prefix}cookie.$login -d "login=$login&pass=$password" http://$login.minitroopers.com/login
+if [ $# -gt 1 ]
+then
+    curl -c ${prefix}cookie.$login -d "login=$login&pass=$password" http://$login.minitroopers.com/login
+else
+    curl -c ${prefix}cookie.$login -d "login=$login" http://$login.minitroopers.com/login
+fi
 curl -b ${prefix}cookie.$login http://$login.minitroopers.com/hq > index
 check
 
 # Make raid tasks
-while [ $message == "Another" ]
+while [ "$exit_cycle" != "1" ]
 do
     key=`egrep -o -e "chk=[A-Za-z0-9]{6}" index |tail -n1`
     curl -b ${prefix}cookie.$login http://$login.minitroopers.com/b/raid?$key
@@ -54,5 +61,5 @@ do
     check
 done
 
-rm -f ./index ./opp # ./cookie.*
+rm -f ./index ./opp ./cookie.*
 exit 0
