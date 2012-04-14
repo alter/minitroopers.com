a33143341a4995b3a6e508faaa463c2b59cd2623
alter <alter.pub@gmail.com>
2012-04-14 23:37:47 +0400

    added ${prefix} to all temporaty files

--- troopers.sh
+++ troopers.sh
@@ -8,7 +8,7 @@ exit_cycle=0
 
 # Check for "raids"
 function check {
-    message=`egrep "(Another|Shortage)" index`
+    message=`egrep "(Another|Shortage)" ${prefix}index`
     if [ "$message" == "Shortage" ] || [ -z "$message" ]
     then
         exit_cycle=1
@@ -18,13 +18,13 @@ function check {
 
 # Get money
 function getmoney {
-    money=`grep money index -A1|tail -n1`
+    money=`grep money ${prefix}index -A1|tail -n1`
     echo "$login has earned $money coins"
 }
 
 # Make 3 "mission" tasks
 function mission {
-    mission_key=`egrep -o -e "chk=[A-Za-z0-9]{6}" index |tail -n1`
+    mission_key=`egrep -o -e "chk=[A-Za-z0-9]{6}" ${prefix}index |tail -n1`
     for i in {1..3}
     do
         curl $curl_opt http://$login.minitroopers.com/b/mission?$mission_key
@@ -36,9 +36,8 @@ function mission {
 function fight {
     for i in {1..3}
     do
-        curl $curl_opt http://$login.minitroopers.com/b/opp > opp
-        fight_key=`egrep -o -e "opp=[0-9]{5,7};chk=[a-zA-Z0-9]{6}" opp|head -n1`
-        echo $fight_key
+        curl $curl_opt http://$login.minitroopers.com/b/opp > ${prefix}opp
+        fight_key=`egrep -o -e "opp=[0-9]{5,7};chk=[a-zA-Z0-9]{6}" ${prefix}opp|head -n1`
         curl $curl_opt http://$login.minitroopers.com/b/battle?$fight_key
     done
 }
@@ -50,17 +49,17 @@ then
 else
     curl $curl_opt -d "login=$login" http://$login.minitroopers.com/login
 fi
-curl $curl_opt http://$login.minitroopers.com/hq > index
+curl $curl_opt http://$login.minitroopers.com/hq > ${prefix}index
 check
 
 # Make raid tasks
 while [ "$exit_cycle" != "1" ]
 do
-    key=`egrep -o -e "chk=[A-Za-z0-9]{6}" index |tail -n1`
+    key=`egrep -o -e "chk=[A-Za-z0-9]{6}" ${prefix}index |tail -n1`
     curl $curl_opt http://$login.minitroopers.com/b/raid?$key
-    curl $curl_opt http://$login.minitroopers.com/hq > index
+    curl $curl_opt http://$login.minitroopers.com/hq > ${prefix}index
     check
 done
 
-rm -f ./index ./opp ./cookie.*
+rm -f ${prefix}index ${prefix}opp ${prefix}cookie.*
 exit 0
