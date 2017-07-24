b91c6f82df3741818a1ab046cd55704f52967d86
alter <alter.pub@gmail.com>
2017-07-24 12:46:42 +0300

    revert changes

--- troopers.sh
+++ troopers.sh
@@ -1,89 +1,76 @@
 #!/bin/bash
 
-trap "cleanup" EXIT
-
+prefix="`dirname $0`/"          # prefix should be with "/" in the end
 login=$1                        # 1st argument of cli
 password=$2                     # 2nd argument of cli
+curl_opt="-s -b ${prefix}cookie.$login -c ${prefix}cookie.$login"
+exit_cycle=0
+selected_enemy=1                # if you want fight with specific avatar
 friend=$3
-site="http://$login.minitroopers.com"
-cookie_file="$(mktemp -t "$login.XXXXXX" --suffix='.cookie')"
-curl="curl --silent --cookie "$cookie_file""
-egrep="grep --extended-regexp --only-matching"
+if [ -z "$friend" ]
+then
+  friend="roushet"             # avatar's name
+fi
 
 # Check for "raids"
-function hasRecruits {
-    $curl "$site/hq" | grep "Another" > /dev/null
-    return $?
+function check {
+    message=`egrep "(Another|Shortage)" ${prefix}index`
+    if [ "$message" == "Shortage" ] || [ -z "$message" ]
+    then
+        exit_cycle=1
+        mission
+    fi
+}
+
+# Get money
+function getmoney {
+    money=`grep money ${prefix}index -A1|tail -n1`
+    echo "$login has earned $money coins"
 }
 
 # Make 3 "mission" tasks
 function mission {
+    mission_key=`egrep -o -e "chk=[A-Za-z0-9]{6}" ${prefix}index |tail -n1`
     for i in {1..3}
     do
-        $curl "$site/b/mission?chk=$chk"
+        curl $curl_opt http://$login.minitroopers.com/b/mission?$mission_key
     done
+    fight
 }
 
-function getCheck {
-    local key="$($egrep --regexp='keyy6:[A-Za-z0-9]{6}y[0-9]:' $1)"
-    echo ${key:6:6}
-}
-
-function getFightKey {
-    $curl --cookie-jar "$cookie_file" "$site/b/opp" \
-        | $egrep --regexp='opp=[0-9]{5,7};chk=[A-Za-z0-9]{6}' \
-        | head --lines=1
-}
-
-function fightRandom {
-    for i in {1..3}; do
-        local key="$(getFightKey)"
-        $curl "$site/b/battle?$key"
-    done
-}
-
-function fightFriend {
-    for i in {1..3}; do
-        $curl "$site/b/battle?chk=$chk&friend=$1"
-    done
-}
-
-function raid {
-    while hasRecruits
+# Make "fight" tasks
+function fight {
+    for i in {1..3}
     do
-        $curl "$site/b/raid?chk=$chk"
+        curl $curl_opt http://$login.minitroopers.com/b/opp > ${prefix}opp
+        fight_key=`egrep -o -e "opp=[0-9]{5,7};chk=[a-zA-Z0-9]{6}" ${prefix}opp|head -n1`
+        if [ $selected_enemy -ne 1 ]
+        then
+            curl $curl_opt http://$login.minitroopers.com/b/battle?$fight_key
+        else
+            curl $curl_opt "http://$login.minitroopers.com/b/battle?$fight_key&friend=$friend"
+        fi
     done
-}
-
-function cleanup {
-    rm --force "$cookie_file"
-}
-
-function login {
-    curl --silent --cookie-jar "$cookie_file" --data "$1" "$site/login"
 }
 
 # Login
-if [[ -n $password ]]
+if [ $# -gt 1 ]
 then
-    login "login=$login&pass=$password"
+    curl $curl_opt -d "login=$login&pass=$password" http://$login.minitroopers.com/login
 else
-    login "login=$login"
+    curl $curl_opt -d "login=$login" http://$login.minitroopers.com/login
 fi
+curl $curl_opt http://$login.minitroopers.com/hq > ${prefix}index
+check
 
-chk="$(getCheck "$cookie_file")"
+# Make raid tasks
+while [ "$exit_cycle" != "1" ]
+do
+    key=`egrep -o -e "chk=[A-Za-z0-9]{6}" ${prefix}index |tail -n1`
+    curl $curl_opt http://$login.minitroopers.com/b/raid?$key
+    curl $curl_opt http://$login.minitroopers.com/hq > ${prefix}index
+    check
+done
 
-# Battle
-if [[ -z "$friend" ]]; then
-    fightRandom
-else
-    fightFriend "$friend"
-fi &
-
-mission &
-
-raid &
-
-wait
-
+rm -f ${prefix}index ${prefix}opp ${prefix}cookie.*
 exit 0
